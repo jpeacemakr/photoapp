e40d4b663360d09b4c182e358d6aec21dcf89973
James Peacemaker <jpeacemakr@gmail.com>
2020-04-01 15:31:28 -0400

    saves photos now 3

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -20,67 +20,67 @@ export function usePhotoGallery() {
 
     
 
+
   useEffect(() => {
-    const loadSaved = async () => {
-      const photosString = await get('photos');
-      const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
-      for (let photo of photos) {
-        const file = await readFile({
-          path: photo.filepath,
-          directory: FilesystemDirectory.Data
-        });
-        photo.base64 = `data:image/jpeg;base64,${file.data}`;
-      }
-      setPhotos(photos);
-    };
-    loadSaved();
-  }, [get, readFile]);
-
-
-    
-  const takePhoto = async () => {
-    const cameraPhoto = await getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
-    });
-
-    const fileName = new Date().getTime() + '.jpeg';
-
-    const newPhotos = [{
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath
-    }, ...photos];
-    setPhotos(newPhotos)
-
-
-    set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
-      // Don't save the base64 representation of the photo data, 
-      // since it's already saved on the Filesystem
-      const photoCopy = { ...p };
-      delete photoCopy.base64;
-      return photoCopy;
-    })));
-
+  const loadSaved = async () => {
+    const photosString = await get('photos');
+    const photos = (photosString ? JSON.parse(photosString) : []) as Photo[];
+    for (let photo of photos) {
+      const file = await readFile({
+        path: photo.filepath,
+        directory: FilesystemDirectory.Data
+      });
+      photo.base64 = `data:image/jpeg;base64,${file.data}`;
+    }
+    setPhotos(photos);
   };
+  loadSaved();
+}, [get, readFile]);
+
+
+
+  const takePhoto = async () => {
+  const cameraPhoto = await getPhoto({
+    resultType: CameraResultType.Uri,
+    source: CameraSource.Camera,
+    quality: 100
+  });
+
+  const fileName = new Date().getTime() + '.jpeg';
+  const savedFileImage = await savePicture(cameraPhoto, fileName);
+  const newPhotos = [savedFileImage, ...photos];
+  setPhotos(newPhotos);
+
+  set(PHOTO_STORAGE, JSON.stringify(newPhotos.map(p => {
+    // Don't save the base64 representation of the photo data, 
+    // since it's already saved on the Filesystem
+    const photoCopy = { ...p };
+    delete photoCopy.base64;
+    return photoCopy;
+  })));
+    
+};
+
 
 
   const savePicture = async (photo: CameraPhoto, fileName: string) => {
-    const base64Data = await base64FromPath(photo.webPath!);
-    await writeFile({
-      path: fileName,
-      data: base64Data,
-      directory: FilesystemDirectory.Data
-    });
-    return getPhotoFile(photo, fileName);
-  };
+  const base64Data = await base64FromPath(photo.webPath!);
+  await writeFile({
+    path: fileName,
+    data: base64Data,
+    directory: FilesystemDirectory.Data
+  });
+  return getPhotoFile(photo, fileName);
+};
 
-  const getPhotoFile = async (cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> => {
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath
-    };
+
+
+const getPhotoFile = async (cameraPhoto: CameraPhoto, fileName: string): Promise<Photo> => {
+  return {
+    filepath: fileName,
+    webviewPath: cameraPhoto.webPath
   };
+};
 
 
   return {
